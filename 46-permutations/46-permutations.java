f04e545145d5dd3ec7125060d8cbdf5555819b1d
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-11-04 11:07:00 +0530

    Time: 2 ms (81.45%), Space: 44.2 MB (78.55%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> list;
+    public List<List<Integer>> permute(int[] nums) {
+        list= new ArrayList<>();
+        permute(0,nums);
+        return list;
+    }
+    void permute(int lo,int[] nums){
+        if(lo==nums.length){
+            List<Integer> sub= new ArrayList<>();
+            for(int val: nums) sub.add(val);
+            list.add(sub);
+        }
+        for(int ind=lo;ind<nums.length;ind++){
+            swap(nums,lo,ind);
+            permute(lo+1,nums);
+            swap(nums,lo,ind);
+        }
+    }
+    void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
