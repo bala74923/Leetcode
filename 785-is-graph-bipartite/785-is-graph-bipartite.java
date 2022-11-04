933595122fd107d51cc57dc43a9f8efd7b48533f
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-11-04 19:45:55 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (96.64%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -4,8 +4,10 @@ class Solution {
         int[] color = new int[N];
         Arrays.fill(color,-1);// let 0,1 be colors
         for(int i=0;i<N;i++){
-            if(color[i]==-1 && !check(graph,color,i)){
-                return false;
+            if(color[i]==-1 ){
+                color[i] = 0;
+                if(dfs(graph,color,i)==false)
+                    return false;
             }
         }
        return true;
@@ -23,6 +25,17 @@ class Solution {
                     color[adj] = 1^color[front];
                     q.add(adj);
                 }
+            }
+        }
+        return true;
+    }
+    boolean dfs(int[][] graph,int[] color,int curr){
+        for(int adj: graph[curr]){
+            if(color[adj]==color[curr]) return false;
+            if(color[adj]==-1){
+                color[adj] = 1^color[curr];
+                if(dfs(graph,color,adj)==false)
+                    return false;
             }
         }
         return true;
