ffe52c80bbb043f43e1a790fc1c34410cf7a7d6f
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-11-05 15:07:47 +0530

    Time: 931 ms (24.41%), Space: 192 MB (5.01%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.java
+++ 212-word-search-ii/212-word-search-ii.java
@@ -1,67 +1,58 @@
 class Trie{
     Trie[] arr;
+    boolean end ;
     String str;
     Trie(){
         this.arr = new Trie[26];
         str = null;
+        end = false;
     }
     void addWord(String s){
         Trie temp = this;
         for(char c: s.toCharArray()){
+            // check is already exist or not
             int ind = c-'a';
-            // if not present add
             if(temp.arr[ind]==null){
                 temp.arr[ind] = new Trie();
-                temp = temp.arr[ind];
-            }else temp = temp.arr[ind];
-            // else skip
+            }
+            temp = temp.arr[ind];
         }
-        // at the last character 
+        temp.end = true;
         temp.str = s;
     }
 }
-
 class Solution {
-     int[][] dirs = {{-1,0},{0,1},{1,0},{0,-1}};
-    Set<String> soln;
+    Set<String> list;
+    char[][] board;
     public List<String> findWords(char[][] board, String[] words) {
-        int m = board.length, n = board[0].length;
         Trie root = new Trie();
-        soln = new HashSet<>();
-        for(String word: words) root.addWord(word);
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                rec(i,j,board,root);
+        for(String s: words) root.addWord(s);
+        list = new HashSet<>();
+        boolean[][] vis = new boolean[board.length][board[0].length];
+        this.board=  board;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                dfs(i,j,vis,root);
             }
         }
-        return new ArrayList<>(soln);
+        return  new ArrayList<>(list);
     }
-    void rec(int i,int j,char[][] board, Trie temp){
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]=='$'|| 
-          temp==null){
-            // is not valid or already visited
-            return ;
-        }
-       
-        char tmpchar = board[i][j];
-        board[i][j] = '$'; // marking it as visited
-        // if valid add the current value
-        // if not present then add else leave it
-        int ind = tmpchar-'a';
-        
-        Trie child = temp.arr[ind]; 
-         if(child!=null && child.str!=null){
-            // we found some character
-            //System.out.println(i+","+j);
-            soln.add(child.str);
-        }
+    void dfs(int i,int j,boolean[][] visited, Trie temp){
+        if(i<0 || j<0|| i>=board.length|| j>=board[0].length ||  visited[i][j]) return ;
+        int ind = board[i][j]-'a';
+        if(temp.arr[ind]==null) return ;
+        visited[i][j] = true;
+        // check is there any element having this
+        temp= temp.arr[ind];
+        //System.out.println(board[i][j]);
+        if(temp.end==true) list.add(temp.str);
+            
+        int[][] dirs ={{-1,0},{0,1},{1,0},{0,-1}};
         for(int[] dir: dirs){
-            int x = i+dir[0], y = j+dir[1];
-            rec(x,y,board,child);
+            int x= dir[0]+i,y =dir[1]+j;
+            dfs(x,y,visited,temp);
         }
-        board[i][j] = tmpchar;
+        
+        visited[i][j] = false;
     }
-    
 }
-// make all possible strings using backtracking
