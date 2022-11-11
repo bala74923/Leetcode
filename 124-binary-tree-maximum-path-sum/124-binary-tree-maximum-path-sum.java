bca16716744524d702534e8b3c760aba9601b758
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-11-11 09:36:54 +0530

    Time: 1 ms (99.60%), Space: 48 MB (34.00%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -22,8 +22,12 @@ class Solution {
     }
      int go(TreeNode root){
         if(root==null) return 0;
+         // we can skip left part if it is negative
         int left = Math.max(go(root.left),0);
+         // we can skip right part if it is negative
         int right= Math.max(go(root.right),0);
+         // we cannot skip the current part because curver os
+         // passing throught the current part
         res = Math.max(res,left+right+root.val);
         return Math.max(left,right)+root.val;
     }
