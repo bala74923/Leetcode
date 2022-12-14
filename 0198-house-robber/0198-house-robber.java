d6c4045e5023daced8c8fd28e5253be3bfe15492
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-14 22:55:33 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (87.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,30 +1,21 @@
 class Solution {
-    int[] mem;
     public int rob(int[] nums) {
-        // int N = nums.length;
-        // mem = new int[N];
-        // Arrays.fill(mem,-1);
         return solve(nums);
     }
     int solve(int[] nums){
          int N = nums.length;
-        int[] dp = new int[N];
-        dp[0] = nums[0];
-        if(N>1) dp[1] = Math.max(nums[0],nums[1]);
+        
+        int prepre = nums[0],pre = prepre;
+        if(N>1) pre = Math.max(nums[0],nums[1]);
         for(int ind=2;ind<N;ind++){
-            int pick  = nums[ind]  + dp[ind-2];
-            int notPick = dp[ind-1];
-            dp[ind] = Math.max(pick,notPick);
+            int pick  = nums[ind]  + prepre;
+            int notPick = pre;
+            int curr = Math.max(pick,notPick);
+            
+            prepre = pre;
+            pre= curr;
         }
-        return dp[N-1];
+        return pre;
     }
-    int f(int ind,int[] nums){
-        if(ind==0) return nums[ind];
-        if(ind==1) return Math.max(nums[0],nums[1]);
-        if(mem[ind]!=-1)
-            return mem[ind];
-        int pick  = nums[ind]  + f(ind-2,nums);
-        int notPick = f(ind-1,nums);
-        return mem[ind] = Math.max(pick,notPick);
-    }
+    
 }
