1f25e875d9b8bbf14c64269a946b6c84f3811a33
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-15 08:13:07 +0530

    Time: 10 ms (98.36%), Space: 39.8 MB (99.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,17 +4,18 @@ class Solution {
     }
     int solve(String s1,String s2){
         int n1 = s1.length(), n2 = s2.length();
-        int[] prev = new int[n2+1];
+        int[] dp = new int[n2+1];
         for(int i=1;i<=n1;i++){
-            int[] curr = new int[n2+1];
+            int prev_state = 0;
             for(int j=1;j<=n2;j++){
+                int curr_state = dp[j];
                 if(s1.charAt(i-1)==s2.charAt(j-1))
-                     curr[j] =  1+ prev[j-1];
+                     dp[j] =  1+ prev_state;
                 else 
-                    curr[j] = Math.max(prev[j],curr[j-1]);
+                    dp[j] = Math.max(dp[j],dp[j-1]);
+                prev_state = curr_state;
             }
-            prev = curr;
         }
-        return prev[n2];
+        return dp[n2];
     }
 }
