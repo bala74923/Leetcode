0323f0c997a73efbab9cc951e4d9f71933bcf370
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-22 11:07:18 +0530

    Time: 691 ms (33.55%), Space: 65.4 MB (76.19%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,24 +1,36 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int sum  =0 , N = points.length;
-        boolean[] visited = new boolean[N];
-        // {wt, node, parent} -> parent for mst
-        // {wt, node} -> sum
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        pq.add(new int[]{0,0});
-        
-        
-        while(!pq.isEmpty()){
-            int[] front = pq.poll();
-            int node = front[1], wt = front[0];
-            if(visited[node]) continue;
-            visited[node] = true;
-            sum+= wt;
-            for(int adj =0;adj< N;adj++){
-                int newDist = Math.abs(points[node][0]-points[adj][0]) + Math.abs(points[node][1]-points[adj][1]);
-                pq.add(new int[]{newDist,adj});
+        for(int i=0;i<points.length;i++){
+            for(int j=i+1;j<points.length;j++){
+                pq.add(new int[]{dist(points,i,j), i, j});
             }
         }
-        return sum;
+        int[] dsuf = new int[points.length];
+        Arrays.fill(dsuf,-1);
+        int mst_sum = 0;
+        while(!pq.isEmpty()){
+            int[] front = pq.poll();
+            if(union(front[1],front[2],dsuf))
+                mst_sum+=front[0];
+        }
+        return mst_sum;
+    }
+    boolean union(int a,int b,int[] dsuf){
+        int pa = find(a,dsuf), pb = find(b,dsuf);
+        if(pa==pb) //edge forms cycle
+            return false;
+        else{
+            dsuf[pa] = pb;
+            return true;
+        }
+    }
+    int dist(int[][] a,int i,int j){
+        return Math.abs(a[i][0]-a[j][0]) + Math.abs(a[i][1]-a[j][1]);
+    }
+    int find(int node,int[] dsuf){
+        if(dsuf[node]==-1)
+            return node;
+        return dsuf[node] = find(dsuf[node],dsuf);
     }
 }
