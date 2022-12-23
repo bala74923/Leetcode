0451ff8bc6adf3a74e9cee7db33643c1cb57201b
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-23 07:32:43 +0530

    Time: 1 ms (90.22%), Space: 40.7 MB (80.79%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -12,39 +12,19 @@ class Solution {
         dp[N][0]= dp[N][1] = dp[N][2] = 0;
         // ind-> 0 to N, prices -> 0 to 2
         for(int ind = N-1;ind>=0;ind--){
-            for(int tag = 2;tag>=0;tag--){
-                if(tag==2){ // cool down
-                    dp[ind][tag] = dp[ind+1][0];
-                }else if( tag==1){ // sell or may not sell
-                    int sell = prices[ind] +dp[ind+1][2];
-                    int not_sell = dp[ind+1][1];
-                    dp[ind][tag] = Math.max(sell, not_sell);
-                }else{
-                    // buy or not buy
-                    int buy = -prices[ind]+ dp[ind+1][1];
-                    int not_buy = dp[ind+1][0];
-                    dp[ind][tag] = Math.max(buy, not_buy);
-                }
-            }
+            // cool down
+            dp[ind][2] = dp[ind+1][0];
+               
+            // sell or may not sell
+            int sell = prices[ind] +dp[ind+1][2];
+            int not_sell = dp[ind+1][1];
+            dp[ind][1] = Math.max(sell, not_sell);
+               
+            // buy or not buy
+            int buy = -prices[ind]+ dp[ind+1][1];
+            int not_buy = dp[ind+1][0];
+            dp[ind][0] = Math.max(buy, not_buy);
         }
         return dp[0][0];
-    }
-    int f(int ind,int tag, int[] prices){
-        if(prices.length ==  ind)
-            return 0;
-        if(mem[ind][tag]!=-1)
-            return mem[ind][tag];
-        if(tag==2){ // cool down
-            return mem[ind][tag] = f(ind+1, 0, prices);
-        }else if( tag==1){ // sell or may not sell
-            int sell = prices[ind] +f(ind+1, 2, prices);
-            int not_sell = f(ind+1, 1, prices);
-            return mem[ind][tag] = Math.max(sell, not_sell);
-        }else{
-            // buy or not buy
-            int buy = -prices[ind]+ f(ind+1, 1, prices);
-            int not_buy = f(ind+1, 0, prices);
-            return mem[ind][tag] = Math.max(buy, not_buy);
-        }
     }
 }
