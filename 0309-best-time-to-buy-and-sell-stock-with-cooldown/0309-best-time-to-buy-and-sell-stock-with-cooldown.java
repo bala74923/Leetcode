0b7ead629dc9fd6906322b8adaa4dc6bf0c185d4
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-23 07:36:44 +0530

    Time: 1 ms (90.22%), Space: 40.7 MB (80.79%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,28 +3,32 @@ class Solution {
     int[][] mem;
     public int maxProfit(int[] prices) {
         return solve(prices.length, prices);
-        
     }
     int solve(int N,int[] prices){
         int[][] dp= new int[N+1][3];
         // ind-> 0 to N, tag-> 0 to 2
         //write base case
         dp[N][0]= dp[N][1] = dp[N][2] = 0;
+        int p_buy = 0, p_sell = 0, p_cool = 0;
         // ind-> 0 to N, prices -> 0 to 2
         for(int ind = N-1;ind>=0;ind--){
             // cool down
-            dp[ind][2] = dp[ind+1][0];
+            int c_cool = p_buy;
                
             // sell or may not sell
-            int sell = prices[ind] +dp[ind+1][2];
-            int not_sell = dp[ind+1][1];
-            dp[ind][1] = Math.max(sell, not_sell);
+            int sell = prices[ind] +p_cool;
+            int not_sell = p_sell;
+            int c_sell = Math.max(sell, not_sell);
                
             // buy or not buy
-            int buy = -prices[ind]+ dp[ind+1][1];
-            int not_buy = dp[ind+1][0];
-            dp[ind][0] = Math.max(buy, not_buy);
+            int buy = -prices[ind]+ p_sell;
+            int not_buy = p_buy;
+            int c_buy = Math.max(buy, not_buy);
+            
+            p_buy = c_buy;
+            p_sell = c_sell;
+            p_cool= c_cool;
         }
-        return dp[0][0];
+        return p_buy;
     }
 }
