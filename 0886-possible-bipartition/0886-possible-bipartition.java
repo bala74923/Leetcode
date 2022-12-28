169cf67d5635582dd36fdf82d9c2b996928c5ff7
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-28 11:24:05 +0530

    Time: 14 ms (90.57%), Space: 50.2 MB (75.61%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,36 +1,40 @@
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0;i<=n;i++)
-            list.add(new ArrayList<>());
-        for(int[] e: dislikes){
-            int from = e[0], to = e[1];
-            list.get(from).add(to);
-            list.get(to).add(from);
-        }
+        List<List<Integer>> list = graph(dislikes,n);
         int[] color = new int[n+1];
-        Arrays.fill(color,-1);
-        // check for all components
-       for(int i=0;i<=n;i++){
-           if(color[i]==-1){
-               color[i] = 0;
-               if(cycle(list, i, color))
-                   return false;
-           }
-       }
+        Arrays.fill(color, -1);
+        for(int i=1;i<=n;i++){
+            if(color[i]==-1){
+                color[i] = 0;
+                if(dfs(i,color,list)){ //return true if component is not bipartite
+                    return false;
+                }
+            }
+        }
         return true;
     }
-    static boolean cycle(List<List<Integer>> list, int curr, int[] color){
-        int newColor = color[curr]^1;
+    boolean dfs(int curr,int[] color,List<List<Integer>> list){
+        int nc = color[curr]^1;
         for(int adj: list.get(curr)){
-            if(color[adj]==color[curr])
-                return true;
+            if(color[adj]!=-1 && color[adj]!=nc)
+                return true;// we cannot do it in bipartite
             else if(color[adj]==-1){
-                color[adj] = newColor;
-                if(cycle(list, adj, color))
+                color[adj] = nc;
+                if(dfs(adj ,color, list))
                     return true;
             }
         }
         return false;
+    }
+    List<List<Integer>> graph(int[][] edges,int n){
+        List<List<Integer>> list= new ArrayList<>();
+        for(int i=0;i<=n;i++)
+            list.add(new ArrayList<>());
+        for(int[] e: edges){
+            int from = e[0], to = e[1];
+            list.get(from).add(to);
+            list.get(to).add(from);
+        }
+        return list;
     }
 }
