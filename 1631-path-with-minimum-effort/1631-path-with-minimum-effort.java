7a98c36513474e932f0e48ff00457f68a1d0a83a
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-28 12:32:52 +0530

    Time: 68 ms (81.42%), Space: 42.6 MB (93.49%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,33 +1,38 @@
+
 class Solution {
+    int[][] dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+    int int_min = -(int)1e9;
     int int_max = (int)1e9;
-    int[][] dirs=  {{-1,0},{0,1},{1,0},{0,-1}};
-    public int minimumEffortPath(int[][] grid) {
-        // not constant distance so dijkstra
-        int M = grid.length, N = grid[0].length;
-        int[][] dp = new int[M][N];
-        for(int i=0;i<M;i++){
-            Arrays.fill(dp[i],int_max);
-        }
+    public int minimumEffortPath(int[][] heights) {
+        //applying dijkstra
+        int M = heights.length, N = heights[0].length;
+        int[][] dp= new int[M][N];
+        // setting all values as int_max
+        for(int[] dp1: dp)
+            Arrays.fill(dp1,int_max);
+        // setting src distance as 0
         dp[0][0] = 0;
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        pq.add(new int[]{0,0,0});
+        //{i,j,max_abs_diff}
+        PriorityQueue<int[]> pq = 
+            new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.add(new int[]{0,0,int_min});
         
         while(!pq.isEmpty()){
             int[] front = pq.poll();
-            int i = front[1],j = front[2];
+            int i = front[0],j = front[1], upto=  front[2];
             for(int[] dir: dirs){
-                int x= front[1]+dir[0], y =front[2]+dir[1];
-                if(x>=0 && y>=0 && x<M && y<N){
-                    int dist  = Math.max( front[0],Math.abs(grid[i][j]-grid[x][y]));
-                    if(dp[x][y]>dist){
-                        dp[x][y] = dist;
-                        pq.add(new int[]{dist,x,y});
+                int x = dir[0]+i,y = dir[1]+j;
+                if(x>-1 && y>-1 && x<M && y<N){ // validity check
+                    //System.out.println(x+" "+y+);
+                    int new_diff=  Math.max( Math.abs(heights[i][j]-heights[x][y]),upto);
+                    if(new_diff< dp[x][y]){
+                        dp[x][y] = new_diff;
+                        pq.add(new int[]{ x, y, new_diff});
                     }
                 }
             }
         }
-        return dp[M-1][N-1];
         
+        return dp[M-1][N-1];
     }
 }
