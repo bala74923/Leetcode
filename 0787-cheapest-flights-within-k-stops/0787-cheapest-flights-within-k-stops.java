6013ed1605008fc35ef5796712ac6d214251e8b9
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-30 12:05:37 +0530

    Time: 5 ms (88.66%), Space: 43.8 MB (67.30%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,35 +1,43 @@
 class Solution {
-    int int_max=  (int)1e9;
+    int int_max = (int)1e9;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> list = new ArrayList<>();
-        for(int i=0;i<n;i++)
-            list.add(new ArrayList<>());
-        for(int[] e: flights){
-            list.get(e[0]).add(new int[]{e[1],e[2]});
-        }
+        List<List<int[]>> list = graph(n, flights);
+        int[] dp = new int[n];
+        Arrays.fill(dp , int_max);
+        dp[src] = 0;
+        // here stops increase unit wise so use queue
+        //{stops , cost, node}
+        PriorityQueue<int[]> pq = 
+            new PriorityQueue<>(
+            (a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
+        pq.add(new int[]{0,0,src});
         
-        int[] dist = new int[n];
-        Arrays.fill(dist,int_max);
-        dist[src] = 0;
-        
-        //{step,distance,node}
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,src});
-        
-        while(!q.isEmpty()){
-            int[] front= q.poll();
-            int step = front[0], d =front[1], node = front[2];
-            if(step>k) continue;
+        while(!pq.isEmpty()){
+            int[] front = pq.poll();
+            int node = front[2], stp = front[0], cost = front[1];
             for(int[] adj: list.get(node)){
-                int nn =adj[0], nd = adj[1];
-                if(nd+d< dist[nn]){
-                    dist[nn] = nd+d;
-                    if(nn!=dst)
-                        q.add(new int[]{step+1,nd+d,nn});
+                int adj_node = adj[0], ncost = cost+ adj[1];
+                if(dp[adj_node] > ncost){
+                    dp[adj_node] = ncost;
+                    // if adj_node is not target but <k stops go ahead
+                    if(stp+1<=k && adj_node!=dst){
+                        pq.add( new int[]{stp+1, ncost, adj_node});
+                    }
                 }
             }
         }
         
-        return dist[dst]>=int_max?-1: dist[dst];
+        
+        return dp[dst]==int_max ? -1: dp[dst];
+    }
+    List<List<int[]>> graph(int n,int[][] edges){
+        List<List<int[]>> list = new ArrayList<>();
+        for(int i=0;i<n;i++)
+            list.add(new ArrayList<>());
+        for(int[] e: edges){
+            int from = e[0], to = e[1], cost = e[2];
+            list.get(from).add( new int[]{to,cost});
+        }
+        return list;
     }
 }
