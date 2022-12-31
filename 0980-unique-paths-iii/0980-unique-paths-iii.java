c51e9a5793bf9711eb8b715e8080d666927afe27
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2022-12-31 11:25:14 +0530

    Time: 1 ms (85.48%), Space: 39.5 MB (93.56%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.java
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -1,11 +1,9 @@
 class Solution {
-   // Stack<String> st;
     int empty_squares = 0;
     int[][] dirs = {{-1,0},{0,-1},{0,1},{1,0}};
     public int uniquePathsIII(int[][] grid) {
         int si= -1,sj =- 1, di = -1,dj = -1;
         empty_squares  = 0;
-       // st = new Stack<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length; j++){
                 if(grid[i][j] == 1){
@@ -16,27 +14,41 @@ class Solution {
             }
         }
         int m = grid.length,n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
+        int[] visited = new int[m];
+        //boolean[][] visited = new boolean[m][n];
         return dfs(si,sj, di,dj, grid, visited);
     }
     // hence m,n <=20 apply dfs
-    int dfs(int ci,int cj,int di,int dj,int[][] grid,boolean[][] visited){
-        if(ci<0 || cj<0 || ci>=grid.length || cj>=grid[0].length|| visited[ci][cj] || grid[ci][cj]==-1) return 0;
-        if(ci==di && cj==dj  ){
-           // System.out.println(st+" es="+empty_squares);
-            return empty_squares==1?1:0;
-        }
+    int dfs(int ci,int cj,int di,int dj,int[][] grid,int[] bit){
+        if(ci<0 || cj<0 || ci>=grid.length || cj>=grid[0].length||
+           isVisited(bit[ci],cj) || grid[ci][cj]==-1) 
+            return 0;
+        
+        if(ci==di && cj==dj  ) return empty_squares==1?1:0;
+        
         empty_squares--; //using one square
-        visited[ci][cj] = true;
+        //visited[ci][cj] = true;
+        bit[ci] = visit(bit[ci],cj);
         int res = 0;
-        //st.push(ci+","+cj);
+        
         for(int[] dir: dirs){
             int x= dir[0]+ci, y = dir[1]+cj;
-            res+=dfs(x,y,di,dj, grid, visited);
+            res+=dfs(x,y,di,dj, grid, bit);
         }
-        visited[ci][cj] = false;
-       // st.pop();
+        
+        //visited[ci][cj] = false;
+         bit[ci] = unvisit(bit[ci],cj);
+        
         empty_squares++;// removing one empty square
         return res;
+    }
+    boolean isVisited(int hash,int n){
+        return (hash&(1<<n))!=0;
+    }
+    int visit(int hash,int n){
+        return (hash|(1<<n));
+    }
+    int unvisit(int hash,int n){
+        return (hash^(1<<n));
     }
 }
