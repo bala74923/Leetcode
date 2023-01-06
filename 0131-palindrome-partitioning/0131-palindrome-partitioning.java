9f8038153d7ccf91ec47b060e858655115c17363
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2023-01-06 17:15:57 +0530

    Time: 19 ms (58.16%), Space: 198.7 MB (7.45%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,46 +1,31 @@
-class Solution{
+class Solution {
+    List<List<String>> soln;
+    List<String> rec_stack;
     public List<List<String>> partition(String s) {
-        List<List<String>> soln  = new ArrayList<>();
-        
-        List<String> curr = new ArrayList<>();
-        curr.add(s.charAt(0)+"");
-        Queue<List<String>> q = new LinkedList<>();
-        q.add(curr);
-        
-        int ind= 1;
-        while(ind<s.length()){
-            int size = q.size();
-           // System.out.println(ind-1);
-            char c = s.charAt(ind);
-            //System.out.println(q+" upcoming = "+c);
-            
-            for(int i=0;i< size;i++){
-                List<String> front = q.poll();
-                String last = front.get(front.size()-1);
-                 // another option is create a new list
-                List<String> new_list = new ArrayList<>(front);
-                new_list.add( c+"" );
-                q.add(new_list);
-                    front.remove(front.size()-1);
-                    front.add(last+c);
-                    q.add(front);
-            } 
-            ind++;
-        }
-        while(!q.isEmpty()){
-            List<String> st =q.poll();
-            if(ispal(st)) soln.add(st);
-        
-        }
+        soln = new ArrayList<>();
+        rec_stack = new ArrayList<>();
+        f(0,s);
         return soln;
     }
-    boolean ispal(List<String> l){
-        for(String s: l){
-            int lo= 0,hi = s.length()-1;
-            while(lo<hi){
-                if(s.charAt(lo++) != s.charAt(hi--))
-                    return false;
-            }
+    void f(int ind,String s){
+        if(ind== s.length()){
+            soln.add(new ArrayList<>(rec_stack));
+            return ;
+        }
+        StringBuilder sb = new StringBuilder("");
+        for(int end=ind;end<s.length();end++){
+            sb.append(s.charAt(end));
+            if(ispal( s, ind, end)){
+                rec_stack.add(sb.toString());
+                f(end+1,  s);
+                rec_stack.remove(rec_stack.size()-1);
+            }    
+        }
+    }
+    boolean ispal(String s,int lo,int hi){
+        while(lo<hi){
+            if(s.charAt(lo++) != s.charAt(hi--))
+                return false;
         }
         return true;
     }
