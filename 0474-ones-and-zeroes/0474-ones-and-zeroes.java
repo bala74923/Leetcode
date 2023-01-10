5d48a065dc1ab7e13547ff225c82a7386fc2e14d
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2023-01-11 02:50:01 +0530

    Time: 61 ms (52.65%), Space: 67.5 MB (52.07%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -4,16 +4,19 @@ class Solution {
     int[][][] dp ;
     public int findMaxForm(String[] strs, int m, int n) {
         int N = strs.length;
-       
+        int[] one = new int[N];
+        for(int i=0;i<N;i++){
+            for(char c: strs[i].toCharArray()){
+                if(c=='1') one[i]++;
+            }
+        }
+        
         int[][][] dp = new int[N+1][m+1][n+1];
         for(int ind = 1; ind<=N;ind++){
             for(int _m =0;_m<= m; _m++){
                 for(int _n=0; _n<=n ; _n++){
-                    int zeros  = 0,ones = 0;
-                    for(char c: strs[ind-1].toCharArray()){
-                        if(c=='0') zeros ++;
-                        else ones++;
-                    }
+                    
+                    int ones =  one[ind-1], zeros  = strs[ind-1].length()-ones;           
                     int pick = int_min;
                     if(zeros<=_m && ones<= _n){
                         pick = 1+ dp[ind-1][_m-zeros][_n-ones];
