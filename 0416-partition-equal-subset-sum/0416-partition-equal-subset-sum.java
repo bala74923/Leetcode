68675128b13ecd18be4ab57df0cd48f419e0295c
BALAMURUGAN K <81730721+bala74923@users.noreply.github.com>
2023-01-11 02:29:09 +0530

    Time: 48 ms (71.31%), Space: 45 MB (61.50%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,5 +1,4 @@
 class Solution {
-    int[][] dp;
     public boolean canPartition(int[] nums) {
         // can we able to form total/2
         int tot = 0;
@@ -8,23 +7,25 @@ class Solution {
         }
         if(tot%2 == 1) return false;
         int need = tot/2, n = nums.length;
-        dp = new int[n][need+1];
-        for(int[] dp1: dp)
-            Arrays.fill(dp1,-1);
-        return f(n-1, need, nums);
-    }
-    boolean f(int ind,int sum,int[] nums){
-        if(sum == 0) return true;
-        if(ind ==0){
-            return sum == 0|| nums[0] == sum;
+      
+        boolean[][] dp = new boolean[n][need+1];
+        // base case
+        dp[0][0] = true; 
+        if(need >=nums[0]){
+            dp[0][nums[0]] = true;
         }
-        if(dp[ind][sum]!=-1)
-            return dp[ind][sum] ==1;
-        boolean pick = false;
-        if(sum>= nums[ind]){
-            pick = f(ind-1, sum-nums[ind], nums);
+        for(int ind = 1;ind<n; ind++){
+            dp[ind][0] = true;
+            for(int sum= 0; sum<=need; sum++){
+                boolean pick = false;
+                if(sum>= nums[ind]){
+                    pick = dp[ind-1][ sum-nums[ind]];
+                }
+                boolean not_pick = dp[ind-1][sum];
+                dp[ind][sum] = pick || not_pick;
+            }
         }
-        boolean not_pick = f(ind-1, sum, nums);
-        return (dp[ind][sum] = (pick || not_pick)?1:0)==1;
+        return dp[n-1][need];
+        
     }
 }
